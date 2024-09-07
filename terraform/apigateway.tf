ac7b2abfc62e0fcda5e75dbe2bcba4808afa69ea
Wael Ramadan <thebigguyis@gmail.com>
2024-09-07 23:18:43 +0300

    apigateway docs

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,14 +1,17 @@
+# Defines the API Gateway REST API for the Lambda function
 resource "aws_api_gateway_rest_api" "webserver-api" {
   name        = "WebserverApi"
   description = "API to call Lambda"
 }
 
+# Creates a resource with a proxy path that allows dynamic routing (e.g., for Lambda integration)
 resource "aws_api_gateway_resource" "webserver-resource" {
   rest_api_id = aws_api_gateway_rest_api.webserver-api.id
   parent_id   = aws_api_gateway_rest_api.webserver-api.root_resource_id
   path_part   = "{proxy+}"
 }
 
+# Defines the HTTP method (ANY) for the API Gateway resource and disables authorization
 resource "aws_api_gateway_method" "webserver-method" {
   rest_api_id   = aws_api_gateway_rest_api.webserver-api.id
   resource_id   = aws_api_gateway_resource.webserver-resource.id
@@ -16,6 +19,7 @@ resource "aws_api_gateway_method" "webserver-method" {
   authorization = "NONE"
 }
 
+# Integrates the API Gateway with an AWS Lambda function using AWS_PROXY (Lambda proxy integration)
 resource "aws_api_gateway_integration" "webserver-integration" {
   rest_api_id             = aws_api_gateway_rest_api.webserver-api.id
   resource_id             = aws_api_gateway_resource.webserver-resource.id
@@ -25,6 +29,7 @@ resource "aws_api_gateway_integration" "webserver-integration" {
   uri                     = module.webserver_lambda.invoke_arn
 }
 
+# Defines the root (/) method for the API Gateway, accepting any HTTP method (ANY)
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id   = aws_api_gateway_rest_api.webserver-api.id
   resource_id   = aws_api_gateway_rest_api.webserver-api.root_resource_id
@@ -32,6 +37,7 @@ resource "aws_api_gateway_method" "proxy_root" {
   authorization = "NONE"
 }
 
+# Integrates the root (/) method with an AWS Lambda function using AWS_PROXY
 resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.webserver-api.id
   resource_id = aws_api_gateway_method.proxy_root.resource_id
@@ -42,6 +48,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
   uri                     = module.webserver_lambda.invoke_arn
 }
 
+# Deploys the API Gateway to a specific stage ("api") after all integrations are set up
 resource "aws_api_gateway_deployment" "webserver-deployment" {
   rest_api_id = aws_api_gateway_rest_api.webserver-api.id
   depends_on = [
@@ -51,6 +58,7 @@ resource "aws_api_gateway_deployment" "webserver-deployment" {
   stage_name = "api"
 }
 
+# Grants API Gateway permission to invoke the Lambda function
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
