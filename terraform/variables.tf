398ca30ff08d8060d7c2733241d399dba36ce9f5
Wael Ramadan <thebigguyis@gmail.com>
2024-09-07 23:11:19 +0300

    adjust variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,24 @@ variable "env" {
   type = string
 }
 
+# CIDR block for the VPC
+variable "cidr_block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+# List of CIDR blocks for public subnets
+variable "public_subnets" {
+  description = "A list of CIDR blocks for the public subnets."
+  type        = list(string)
+}
+
+# List of CIDR blocks for private subnets
+variable "private_subnets" {
+  description = "A list of CIDR blocks for the private subnets."
+  type        = list(string)
+}
+
 variable "aws_region" {
   type = string
 }
